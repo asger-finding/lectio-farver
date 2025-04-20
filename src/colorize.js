fab2b09fe257176145a4d1060b07f6bdeb6cc1b0
Asger Finding <asger.finding@gmail.com>
2025-04-20 22:54:17 +0200

    feat: add class sorting feat: add individual removal options refactor: remove Ctrl+Shift+F shortcut

--- src/colorize.js
+++ src/colorize.js
@@ -1,41 +1,65 @@
 (() => {
-	let fagListe = { size: 0 };
 
-	chrome.storage.sync.get(['keyFagListe'], result => {
-		if (result.keyFagListe) fagListe = result.keyFagListe;
+	let colors = {};
+
+	const rgbToHex = (r, g, b) => `#${ r.toString(16) }${ g.toString(16) }${ b.toString(16) }`;
+
+	const getCurrentClass = () => {
+		const regex = /,\s*([^-]+)/;
+		const identifier = document.title.match(regex);
+
+		if (identifier) {
+			const extract = identifier[1].trim();
+			if (extract.length === 0) return null;
+
+			return extract;
+		}
+		return null;
+	}
+
+	const setModuleColor = (element, rgb) => {
+		const [ r, g, b ] = rgb;
+		const color = rgbToHex(r, g, b);
+
+		element.parentElement.style.backgroundImage = 'none';
+		element.parentElement.style.backgroundColor = color;
+		element.style.backgroundColor = color;
+
+		const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+		if (yiq >= 128) {
+			element.style.color = '#000000';
+			element.style.fontWeight = 'inherit';
+		} else {
+			element.style.color = '#ffffff';
+			element.style.fontWeight = 'bold';
+		}
+	}
+
+	chrome.storage.local.set({
+		currentClass: getCurrentClass()
+	});
+
+	chrome.storage.sync.get('assignedColors', ({ assignedColors }) => {
+		if (assignedColors) colors = assignedColors;
 	
 		document.querySelectorAll('.s2skemabrikcontent span[data-lectiocontextcard]:first-child').forEach(span => {
-			const fag = span.textContent.replaceAll(' ', '_');
+			const subject = span.textContent.replaceAll(' ', '_');
 	
-			if (!fagListe[fag]) {
-				const index = fagListe.size;
+			if (!colors[subject]) {
+				const index = Object.keys(colors).length;
 	
 				const r = (90 + index % 5 * 40);
 				const g = (130 + index % 3 * 60);
 				const b = (150 + index % 2 * 100);
 	
-				fagListe[fag] = [r, g, b];
-				fagListe.size++;
+				colors[subject] = [r, g, b];
 			}
-	
-			const [ r, g, b ] = fagListe[fag];
-			const color = `#${ r.toString(16) }${ g.toString(16) }${ b.toString(16) }`;
-	
-			const skemaBrikContent = span.parentElement;
-			skemaBrikContent.parentElement.style.backgroundImage = 'none';
-			skemaBrikContent.parentElement.style.backgroundColor = color;
-			skemaBrikContent.style.backgroundColor = color;
-	
-			const yiq = (r * 299 + g * 587 + b * 114) / 1000;
-			if (yiq >= 128) {
-				skemaBrikContent.style.color = '#000000';
-				skemaBrikContent.style.fontWeight = 'inherit';
-			} else {
-				skemaBrikContent.style.color = '#ffffff';
-				skemaBrikContent.style.fontWeight = 'bold';
-			}
+
+			const subjectElement = span.parentElement;
+			setModuleColor(subjectElement, colors[subject]);
 		});
 	
-		chrome.storage.sync.set({ keyFagListe: fagListe });
+		chrome.storage.sync.set({ assignedColors: colors });
 	});
+
 })();
--- src/editor.js
+++ src/editor.js
@@ -1,52 +1,95 @@
-addEventListener('load', () => {
-	let fagListe = { size: 0 };
+let colors = {};
 
-	const hexToRgb = hex => ['0x' + hex[1] + hex[2] | 0, '0x' + hex[3] + hex[4] | 0, '0x' + hex[5] + hex[6] | 0];
+const hexToRgb = hex => ['0x' + hex[1] + hex[2] | 0, '0x' + hex[3] + hex[4] | 0, '0x' + hex[5] + hex[6] | 0];
 
-	const executeColorScript = () => chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-		if (tabs[0].id) {
-			chrome.scripting.executeScript({
-				target: { tabId: tabs[0].id },
-				files: ['colorize.js']
+const executeColorScript = () => chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+	if (tabs[0].id) {
+		chrome.scripting.executeScript({
+			target: { tabId: tabs[0].id },
+			files: ['colorize.js']
+		});
+	}
+});
+
+const createItem = (subjectIdentifier, rgb) => {
+	const [ r, g, b ] = rgb;
+	const color = `#${ r.toString(16) }${ g.toString(16) }${ b.toString(16) }`;
+
+	const wrapper = document.createElement('div');
+	const input = document.createElement('input');
+	const label = document.createElement('label');
+	const remove = document.createElement('div');
+	const trashIcon = document.createElement('img');
+
+	wrapper.id = subjectIdentifier;
+	wrapper.classList.add('color');
+
+	input.type = 'color';
+	input.id = `${subjectIdentifier}_input`;
+	input.value = color;
+
+	label.setAttribute('for', input.id);
+	label.innerText = subjectIdentifier.replaceAll('_', ' ');
+
+	input.addEventListener('change', evt => {
+		const newColorHex = evt.target.value;
+		const rgb = hexToRgb(newColorHex);
+
+		colors[subjectIdentifier] = rgb;
+
+		chrome.storage.sync.set({ assignedColors: colors }, () => executeColorScript());
+	});
+
+	remove.classList.add('remove');
+	trashIcon.src = './trash.svg';
+
+	remove.addEventListener('mouseup', ({ currentTarget }) => {
+		if (currentTarget) {
+			chrome.storage.sync.get('assignedColors', ({ assignedColors }) => {
+				if (assignedColors) colors = assignedColors;
+
+				delete colors[currentTarget.parentElement.id];
+
+				chrome.storage.sync.set({
+					assignedColors: colors
+				});
+
+				currentTarget.parentElement.remove();
 			});
 		}
 	});
-	
-	chrome.storage.sync.get(['keyFagListe'], result => {
-		if (result.keyFagListe) fagListe = result.keyFagListe;
-	
-		for (const fag in fagListe) {
-			if (fag === 'size') return;
-	
-			const [ r, g, b ] = fagListe[fag];
-			const color = `#${ r.toString(16) }${ g.toString(16) }${ b.toString(16) }`;
-	
-			const wrapper = document.createElement('div');
-			const input = document.createElement('input');
-			const label = document.createElement('label');
 
-			wrapper.classList.add('color');
+	remove.append(trashIcon);
+	wrapper.append(input, label, remove);
+
+	return wrapper;
+}
+
+addEventListener('load', () => {
+	chrome.storage.sync.get('assignedColors', ({ assignedColors }) => {
+		if (assignedColors) colors = assignedColors;
+
+		chrome.storage.local.get('currentClass', ({ currentClass }) => {
+			const colorWrapper = document.getElementById('colors');
+			if (currentClass) {
+				const currentClassSubjects = [];
+				const otherClassSubjects = [];
+				for (const subject in colors) {
+					const item = createItem(subject, colors[subject]);
 	
-			input.type = 'color';
-			input.id = fag;
-			input.value = color;
-	
-			label.setAttribute('for', fag);
-			label.innerText = fag.replaceAll('_', ' ');
-	
-			input.addEventListener('change', evt => {
-				const newColorHex = evt.target.value;
-				const rgb = hexToRgb(newColorHex);
-	
-				fagListe[fag] = rgb;
-	
-				chrome.storage.sync.set({ keyFagListe: fagListe }, () => executeColorScript());
-			});
-	
-			wrapper.append(input, label);
-	
-			document.getElementById('colors').append(wrapper);
-		}
+					if (subject.startsWith(currentClass)) currentClassSubjects.push(item)
+					else otherClassSubjects.push(item);
+				}
+
+				colorWrapper.append(...[
+					...currentClassSubjects,
+					document.createElement('hr'),
+					...otherClassSubjects
+				]);
+			} else {
+				for (const subject in colors) colorWrapper.append(createItem(subject, colors[subject]));
+			}
+		});
 	});
 
 	document.getElementById('reset').addEventListener('mouseup', () => {
@@ -54,5 +97,5 @@ addEventListener('load', () => {
 			executeColorScript();
 			window.close();
 		});
-	})
+	});
 });
